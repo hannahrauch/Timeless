7855c5a04b6d1d814f09f17a763188be04530e26
Ishita Aradhya <aradhyaish@gmail.com>
2025-04-06 05:41:00 -0500

    updated cart page with delete function

--- TimelessFashion/app/(tabs)/cart.tsx
+++ TimelessFashion/app/(tabs)/cart.tsx
@@ -1,12 +1,23 @@
-import { Text, StyleSheet,TextInput, View } from "react-native";
+import { Text, StyleSheet,Image, TouchableOpacity, View } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useState } from 'react';
 
 export default function Cart() {
+  const [items, setItems] = useState([
+    { id: '1', title: 'Green blouse', tag: 'Vintage', type: 'Top', image: 'https://th.bing.com/th/id/OIP.Cet7Hd9-jbva_ZCOpjCFwQAAAA?rs=1&pid=ImgDetMain', price: '$10'},
+    { id: '2', title: 'spiffy shoes', tag: 'Trending', type: 'Accessories', image: 'https://th.bing.com/th/id/OIP.Cet7Hd9-jbva_ZCOpjCFwQAAAA?rs=1&pid=ImgDetMain', price: '$20'},
+    { id: '3', title: 'joe mama pants', tag: 'Upcoming', type: 'Bottom', image: 'https://th.bing.com/th/id/OIP.Cet7Hd9-jbva_ZCOpjCFwQAAAA?rs=1&pid=ImgDetMain', price: '$30'},
+  ]);
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => prevItems.filter(item => item.id !== id));
+  };
+
   return (
     <View
       style={{
         flex: 1,
         alignItems: "center",
-        backgroundColor: "#DB658D",
         backgroundColor: "#DB658D",
       }}
     >
@@ -14,6 +25,7 @@ export default function Cart() {
         <Text style={{
         color:'white',
         fontSize: 32,
+        fontFamily: 'Bio Rhyme SemiBold',
         }}>
         Cart
         </Text>
@@ -23,18 +35,30 @@ export default function Cart() {
         style={styles.inputContainer}
         >
       </View>
-      <View
+
+      {items.map((item,index) => (
+        <View key={index}
         style={styles.viewBox}
         >
+        <Image
+          source={{uri: 'https://th.bing.com/th/id/OIP.Cet7Hd9-jbva_ZCOpjCFwQAAAA?rs=1&pid=ImgDetMain'}}
+          style={styles.image}
+        />
+        <View>
+          <Text style={{fontFamily: 'Abhaya Libre Regular', fontSize: 23}}>
+            {item.tag}
+          </Text>
+          <Text style={{fontFamily: 'Abhaya Libre Regular', fontSize: 23}}>
+            {item.title}
+          </Text>
+        </View>
+        <TouchableOpacity style={{position: 'absolute', right: 10, top: 10}}
+        onPress={()=>deleteItem(item.id)}>
+          <Ionicons name="close-circle-outline" size={23} color={'gray'} />
+        </TouchableOpacity>
       </View>
-      <View
-        style={styles.viewBox1}
-        >
-      </View>
-      <View
-      style={styles.viewBox2}
-      >
-      </View>
+      ))}
+      
       <View
       style={styles.checkout}
       >
@@ -43,12 +67,12 @@ export default function Cart() {
           fontFamily: 'Bio Rhyme SemiBold',
           fontSize: 18,
           marginTop: 10,
-
+          marginLeft: 10,
         }}
         >
           Total: $10.00
         </Text>
-        <View
+        <TouchableOpacity
         style={styles.checkoutBox}
         >
           <Text
@@ -60,7 +84,7 @@ export default function Cart() {
             Checkout 
           </Text>
 
-        </View>
+        </TouchableOpacity>
       </View>
 
 
@@ -91,28 +115,17 @@ const styles = StyleSheet.create({
       height: 125,
       width: '80%',
       bottom: 500,
+      marginBottom: 10,
+      flexDirection: 'row',
+      padding: 10,
       
   },
-  viewBox1: {
-    backgroundColor: 'white',
-    borderRadius: 5,
-    height: 125,
-    width: '80%',
-    bottom: 480,
-  },
-    viewBox2: {
-      backgroundColor: 'white',
-      borderRadius: 5,
-      height: 125,
-      width: '80%',
-      bottom: 460,
-    },
     checkout: {
       backgroundColor: 'white',
       height: 50,
       width: '90%',
-      bottom: 425,
-      
+      bottom: 35,
+      position: 'absolute',
     },
     checkoutBox: {
       backgroundColor: "#F9D9DA",
@@ -123,6 +136,10 @@ const styles = StyleSheet.create({
       bottom: 28,
       justifyContent: 'center',
       alignItems: 'center',
-    }
-
+    },
+    image: {
+      height: 100,
+      width: 80,
+      borderRadius: 5,
+  },
 });
